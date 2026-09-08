704ed3409d101315e8e0e5d16bb502197cc9464c
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2026-09-08 15:45:50 +0200

    pipeline: move End Of Stream to component
    DP components cannot access pipeline objects. Move the End Of Stream
    flag to the component type.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/audio/module_adapter/module/cadence.c
+++ src/audio/module_adapter/module/cadence.c
@@ -551,7 +551,7 @@ int cadence_codec_process_data(struct processing_module *mod,
 		return 0;
 	}
 
-	if (dev->pipeline->expect_eos) {
+	if (dev->expect_eos) {
 		/* Signal that the stream is expected to end anytime soon */
 		API_CALL(cd, XA_API_CMD_INPUT_OVER, 0, NULL, ret);
 		if (ret != LIB_NO_ERROR) {
@@ -606,7 +606,7 @@ int cadence_codec_process_data(struct processing_module *mod,
 		return ret;
 	}
 
-	if (dev->pipeline->expect_eos) {
+	if (dev->expect_eos) {
 		/*
 		 * AAC decoder cannot signal DONE, check if it stopped
 		 * producing data when EOS is expected
